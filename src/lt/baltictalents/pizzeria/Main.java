2f69d734cff463cf92c44985f98802fb6e2786cc
JustasKa <Justas.Karlonas@gmail.com>
2018-08-20 14:55:48 +0300

    Initial commit

---
+++ src/lt/baltictalents/pizzeria/Main.java
@@ -0,0 +1,9 @@
+package lt.baltictalents.pizzeria;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
